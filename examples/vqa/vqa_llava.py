cd5920e8ae18c5d5b5c338151716871b4635443c
donghyun <mpolio2@kyonggi.ac.kr>
2024-08-08 01:42:08 +0000

    ✨ update vqa examples

--- examples/vqa/vqa_llava.py
+++ examples/vqa/vqa_llava.py
@@ -2,28 +2,16 @@ import requests
 from PIL import Image
 
 import torch
-from transformers import LlavaForConditionalGeneration
-from radio_vision_tower import RADIOVisionTower
-from examples.common import load_model
-from easydict import EasyDict
+from transformers import AutoProcessor, LlavaForConditionalGeneration
 
-args = {'mm_im_crop': False}
-args = EasyDict(args)
-
-# radio:<image_size>:<checkpoint_or_version>:<extra_config>
-radio_vision_tower = RADIOVisionTower('radio:432:radio_v2', args)
-
-model_id = "liuhaotian/llava-v1.5-7b"
-# model_id = "llava-hf/llava-1.5-7b-hf"
+model_id = "llava-hf/llava-1.5-7b-hf"
 model = LlavaForConditionalGeneration.from_pretrained(
     model_id, 
     torch_dtype=torch.float16, 
     low_cpu_mem_usage=True, 
 ).to(0)
 
-model.vision_tower = radio_vision_tower
-
-processor = radio_vision_tower.make_preprocessor_external()
+processor = AutoProcessor.from_pretrained(model_id)
 
 # Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
@@ -32,7 +20,7 @@ conversation = [
 
       "role": "user",
       "content": [
-          {"type": "text", "text": "What are these?"},
+          {"type": "text", "text": "What are the kind of cats?"},
           {"type": "image"},
         ],
     },
--- examples/vqa/vqa_radio.py
+++ examples/vqa/vqa_radio.py
@@ -9,31 +9,31 @@ import requests
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_path = "llava-hf/llava-1.5-7b-hf" # "liuhaotian/llava-v1.5-7b"
-args = {'mm_im_crop': True} # for  CLIPImageProcessor
-args = EasyDict(args)
-
 model = LlavaForConditionalGeneration.from_pretrained(
     model_path, 
     torch_dtype=torch.float16, 
     low_cpu_mem_usage=True, 
 ).to(0)
 
-processor2 = AutoProcessor.from_pretrained(model_path)
-
+processor_llava = AutoProcessor.from_pretrained(model_path)
+args = {'mm_im_crop': True} # for CLIPImageProcessor
+args = EasyDict(args)
 # # radio:<image_size>:<checkpoint_or_version>:<extra_config>
-vision_tower = RADIOVisionTower('radio:432:radio_v1', args)
+vision_tower = RADIOVisionTower('radio:336:radio_v2', args)
 vision_tower.to(device=device, dtype=torch.float16)
 
 image_processor = vision_tower.image_processor
 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
-processor = LlavaProcessor(image_processor, tokenizer, chat_template=processor2.chat_template)
+processor = LlavaProcessor(image_processor, tokenizer, chat_template=processor_llava.chat_template)
 
+# Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
+# Each value in "content" has to be a list of dicts with types ("text", "image") 
 conversation = [
     {
 
       "role": "user",
       "content": [
-          {"type": "text", "text": "What are these?"},
+          {"type": "text", "text": "What are the kind of cats?"},
           {"type": "image"},
         ],
     },
@@ -44,5 +44,6 @@ prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
 inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
+
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
